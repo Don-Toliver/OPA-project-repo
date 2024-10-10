bd76c9332a622357621a70464fdaab890449a6a2
Don-Toliver <malpani.spandan@gmail.com>
2024-10-10 15:30:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-secure-bucket-ap-south-1-cicd"  # Replace with your actual S3 bucket name
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+  }
+}
+ 
 provider "aws" {
   region = "ap-south-1"
 }
-
+ 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-secure-bucket-ap-south-1-cicd"
-
+  bucket = "my-cicd-bucket-ap-south-1"
+ 
   tags = {
-    Name        = "my-secure-bucket-ap-south-1-cicd"
-    Environment = "Test-violation-check"
+    Name        = "my-secure-bucket"
+    Environment = "Test"
   }
-
 }
 
 
