b86db615fc19041b7e8955fd9564072a2d718b5c
Don-Toliver <malpani.spandan@gmail.com>
2024-10-10 11:13:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "automation-cicd-pro-ap-south-1-pzrctulv"
-  
-  # Add tags for the bucket
+
   tags = {
     Name        = "automation-cicd-bucket-final"
     Environment = "Test"
   }
 
 }
+
 
