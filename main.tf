e5f11dd1a25bc1cdb48f5aaf36b84847f488d3c6
Don-Toliver <malpani.spandan@gmail.com>
2024-10-10 11:55:20 +0530

    Update_violation_check main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "example" {
   bucket = "automation-cicd-pro-ap-south-1"
 
   tags = {
-    Name        = "automation-cicd-bucket"
+    Name        = "automation-cicd-bucket-violation"
     Environment = "Test"
   }
 
